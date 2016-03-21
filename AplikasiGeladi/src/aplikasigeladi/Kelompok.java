9acd3ace46c94184cb7714afc323181fc5fd6af4
Arfiatna <Arfiatna@Arfiatna.local>
2016-03-21 17:16:53 +0700

    Cek lagi, masih ngarang

--- AplikasiGeladi/src/aplikasigeladi/Kelompok.java
+++ AplikasiGeladi/src/aplikasigeladi/Kelompok.java
@@ -10,5 +10,34 @@ package aplikasigeladi;
  * @author Arfiatna
  */
 public class Kelompok {
-    
+    private int nomor;
+	private Mahasiswa[] anggota;
+	private int nAnggota;
+	
+	
+	public Kelompok(int nomor, int n){
+		this.nomor=nomor;
+		this.anggota=new Mahasiswa[n];
+	}
+	
+	public void addAnggota(Mahasiswa m){
+		if (anggota.length>nAnggota){
+			anggota[nAnggota] = m;
+			nAnggota++;
+		} else {
+			System.out.println("Kelompok sudah penuh!");
+		}
+	}
+	
+	public Mahasiswa getAnggota(int n){
+		if (n<nAnggota){
+			return anggota[n];
+		} else {
+			return null;
+		}
+	}
+	
+	public int getNAnggota(){
+		return this.nAnggota;
+	}
 }
--- AplikasiGeladi/src/aplikasigeladi/Lokasi.java
+++ AplikasiGeladi/src/aplikasigeladi/Lokasi.java
@@ -10,5 +10,78 @@ package aplikasigeladi;
  * @author Arfiatna
  */
 public class Lokasi {
+	private String daerah;
+	private String namaPerusahaan;
+	private Pembimbing[] pembimbing;
+	private int nPembimbing;
+	private Kelompok[] kelompok = new Kelompok[100];
+	private int nKelompok;
+	
+	public Lokasi(String daerah, String namaPerusahaan, int n){
+		this.daerah=daerah;
+		this.namaPerusahaan=namaPerusahaan;
+		this.pembimbing=new Pembimbing[n];
+	}
     
+	public void setDaerah(String daerah){
+		this.daerah = daerah;
+	}
+	
+	public String getDaerah(){
+		return daerah;
+	}
+	
+	public void setNamaPerusahaan(String namaPerusahaan){
+		this.namaPerusahaan = namaPerusahaan;
+	}
+	
+	public String getNamaPerusahaan(){
+		return namaPerusahaan;
+	}
+	
+	public void addPembimbing (Pembimbing p){
+		if (pembimbing.length>nPembimbing){
+			pembimbing[nPembimbing] = p;
+			nPembimbing++;
+		} else {
+			System.out.println("Sudah punya pembimbing");
+		}
+	}
+	
+		public Pembimbing getPembimbing(int n){
+		if (n<nPembimbing){
+			return pembimbing[n];
+		} else {
+			return null;
+		}
+	}
+	
+	
+	public int getNPembimbing(){
+		return this.nPembimbing;
+	}
+	
+	
+	public void addKelompok(int nomor, int n){
+		if (nKelompok<100){
+			kelompok[nKelompok] = new Kelompok(nomor,n);
+			nKelompok++;
+		} else {
+			System.out.println("Kuota kelompok sudah penuh");
+		}
+	}
+	
+	public Kelompok getKelompok(int n){
+		if (n<100){
+			return kelompok[n];
+		} else {
+			return null;
+		}
+	}
+	
+	public int getNKelompok(){
+		return nKelompok;
+	}
+	
+	
 }
