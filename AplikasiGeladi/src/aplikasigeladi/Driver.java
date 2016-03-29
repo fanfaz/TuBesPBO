ec3384414a0f6bde0bf7eacd474c1fd6579c9238
Arfiatna <Arfiatna@Arfiatna.local>
2016-03-29 10:28:31 +0700

    KERJAIN ATUH EUY

--- AplikasiGeladi/src/aplikasigeladi/Driver.java
+++ AplikasiGeladi/src/aplikasigeladi/Driver.java
@@ -1,4 +1,4 @@
-tun/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,15 +7,8 @@ package aplikasigeladi;
 
 /**
  *
- * @author Arfiatna
+ * @author Arfia
  */
-public class AplikasiGeladi {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Driver {
     
 }
